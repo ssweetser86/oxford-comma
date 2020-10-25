32b2e761c1278da393bb846d35097876fcd7bcbc
Stephen <stephenjsweets@gmail.com>
2020-10-25 19:01:23 -0400

    commit tree 8dc67bda02c15c6604e0c1d44aaa9349e9632daa

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,18 @@
 def oxford_comma(array)
+    string = ""
+    length = array.length
+
+    if length == 1
+        return array[0]
+    elsif length == 2
+        array.join(" and ")
+    else
+        i = 0
+        while i+1 < length do
+            string << array[i] + ", "
+            i += 1
+        end
+        string << "and " + array[i]
+    end
 
 end
